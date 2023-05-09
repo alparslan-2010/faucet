0fe8b3dd8315a561eff1713d48052d0c7491f370
alparslan-2010 <alparslannihatt@gmail.com>
2023-05-09 22:36:58 +0200

    faucet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {ethers} from "ethers";
+import {useState, useEffect} from "react";
+import Swal from 'sweetalert2'
+
+
+import FaucetAbi from "./abis/Faucet.json";
+
+const faucetContractAddres = "0x5b4389750cd518e28E1c63CDE9a8d6a973484261"
 function App() {
+
+  const[walletAddress,setWalletAddress] = useState("");
+  const[provider,setProvider] = useState("");
+  
+   useEffect(() => {
+      connectWallet();
+   },[walletAddress]);
+
+
+  const connectWallet = async () => {
+    if(typeof window.ethereum != "undefined"){
+      try{
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await provider.send("eth_requestAccounts");
+
+        setProvider(provider);
+        setWalletAddress(accounts[0]);
+
+      } catch(err){
+        console.log(err);
+      }
+    }
+  }
+
+  const getTokens = async () => {
+
+    try{
+
+      const contract = new ethers.Contract(faucetContractAddres, FaucetAbi,provider.getSigner());
+
+      const transaction  = await contract.requestToken();
+  
+      console.log("contract",contract)
+    }catch (err){
+
+// CommonJS
+const Swal = require('sweetalert2')
+Swal.fire({
+  title: 'Error!',
+  text: 'Do you want to continue',
+  icon: 'error',
+  confirmButtonText: 'Cool'
+})
+      
+    }
+   
+   
+  }
+
+ 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav className="navbar">
+      <div className="container">
+          <div className="navbar-brand">
+            <h1 className="navbar-item is-size-4">MyToken (MTK) Faucet</h1>
+          </div>
+          <div id="navbar-menu" className="navbar-menu">
+            <div className="navbar-end">
+              <button className="button is-white connect-wallet" onClick={connectWallet}>
+                
+                {walletAddress ? `Connected: ${walletAddress.substring(0,6)}... ${walletAddress.substring(38)}` : "Connect Wallet"}
+                </button>
+            </div>
+          </div>
+      </div>
+    </nav>
+
+    <section className='hero'>
+      <div className='faucet-hero-body'>
+          <div className='box'>
+            <input 
+              type="text" 
+              className="input" 
+              placeholder='Enter your wallet address (0x....)'
+              defaultValue={walletAddress}
+              />
+            <button className='button' onClick={getTokens}>Get Tokens</button>
+          </div>
+      </div>
+    </section>
+    </>
   );
 }
 
